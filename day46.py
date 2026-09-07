60257ed539b900e839561829668875603a060e3a
abinaya <karthikradha113@gmail.com>
2026-09-07 20:07:03 +0530

    "day 46 problem"

---
+++ day46.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums):
+        result = []
+
+        def backtrack(path, used):
+            # If permutation is complete
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if not used[i]:
+                    # Choose
+                    path.append(nums[i])
+                    used[i] = True
+
+                    # Explore
+                    backtrack(path, used)
+
+                    # Undo choice
+                    path.pop()
+                    used[i] = False
+
+        backtrack([], [False] * len(nums))
+        return result
