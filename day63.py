aaeac198a6d6f038911a32d147252aca3fbed149
abinaya <karthikradha113@gmail.com>
2026-09-20 13:38:40 +0530

    "day 63 problem"

---
+++ day63.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        dp = [0] * len(obstacleGrid[0])
+        dp[0] = 1
+
+        for row in obstacleGrid:
+            for j in range(len(row)):
+                if row[j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j - 1]
+
+        return dp[-1]
